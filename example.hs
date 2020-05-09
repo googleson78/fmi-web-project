bd39cdf888900b48ecbf4324e30f2ec793f892e2
Georgi Lyubenov <godzbanebane@gmail.com>
2020-05-10 02:01:04 +0300

    Updated example.hs with client and server

--- example.hs
+++ example.hs
@@ -1,28 +1,76 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
 
+module Example where
+
+import Control.Monad.IO.Class (liftIO)
 import Data.Aeson
-import Servant.API
+import Data.IORef
 import Data.Proxy
+import Network.HTTP.Client (newManager, defaultManagerSettings)
+import Network.Wai.Handler.Warp (run)
+import Servant
+import Servant.API
+import Servant.Client
 import Servant.Server
+import System.IO.Unsafe
 
 newtype CounterVal = CounterVal {getCounterVal :: Int}
-  deriving (Num, FromJSON, ToJSON)
+  deriving (Show, Num, FromJSON, ToJSON)
+
+type Counter = GetCounter :<|> StepCounter
 
 type GetCounter = Get '[JSON] CounterVal
-type StepCounter = "step" :> PostNoContent
-type Counter = GetCounter :<|> StepCounter
+type StepCounter = "step" :> Header "User" String :> ReqBody '[JSON] CounterVal :> PostNoContent
+
+server :: Application
+server = serve api counter
+
+api :: Proxy Counter
+api = Proxy
 
 counter :: Server Counter
 counter = getCounter :<|> stepCounter
 
 getCounter :: Server GetCounter
-getCounter = pure $ CounterVal 5
+getCounter = readCounter
 
 stepCounter :: Server StepCounter
-stepCounter = pure NoContent
+stepCounter muser amount =
+  case muser of
+    Nothing -> throwError err401 { errBody = "You must identify yourself via the \"User\" header." }
+    Just user ->
+      if user == "Pesho"
+      then do
+        incrementCounter amount
+        pure NoContent
+      else throwError err403 { errBody = "Only Pesho can modify the counter." }
 
-server :: Application
-server = serve (Proxy @Counter) counter
+startServer :: IO ()
+startServer = do
+  putStrLn "Starting a server"
+  run 8080 server
+
+incrementCounter :: CounterVal -> Handler ()
+incrementCounter n = liftIO $ atomicModifyIORef' globalCounter $ \old -> (old + n, ())
+
+readCounter :: Handler CounterVal
+readCounter = liftIO $ readIORef globalCounter
+
+globalCounter :: IORef CounterVal
+globalCounter = unsafePerformIO $ newIORef $ CounterVal 0
+{-# NOINLINE globalCounter #-}
+
+reqGetCounter :: Client ClientM GetCounter
+reqGetCounter = let f :<|> _ = client api in f
+
+reqStepCounter :: Client ClientM StepCounter
+reqStepCounter = let _ :<|> f = client api in f
+
+defaultClientEnv :: ClientEnv
+defaultClientEnv = unsafePerformIO $ do
+  manager <- newManager defaultManagerSettings
+  pure $ mkClientEnv manager $ (BaseUrl Http "localhost" 8080 "")
+{-# NOINLINE defaultClientEnv #-}
